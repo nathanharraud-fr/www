6586cf776ca1b869ecbb184f706859ad1e3b4546
Nathan HARRAUD <nharraud@gmail.com>
2022-01-27 21:54:07 +0100

    Moved css variables to styles

--- src/App.vue
+++ src/App.vue
@@ -16,25 +16,3 @@ export default {
   </div>
   <router-view/>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: var(--body-color);
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
